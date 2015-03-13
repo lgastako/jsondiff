401a7c13b1d51d8dd1f3f1c633c6f3e1e581fd71
John Evans <lgastako@gmail.com>
2015-03-13 11:06:27 -0700

    Initial working solution.

--- src/jsondiff/core.clj
+++ src/jsondiff/core.clj
@@ -1,6 +1,15 @@
-(ns jsondiff.core)
+(ns jsondiff.core
+  (:require [clojure.data :as data]
+            [clojure.data.json :as json]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn jsondiff [json1 json2]
+  (let [json->edn #(json/read-str % :key-fn keyword)
+        edn->json #(json/write-str %)
+        edn1 (json->edn json1)
+        edn2 (json->edn json2)
+        edn-diff (data/diff edn1 edn2)]
+    (edn->json edn-diff)))
+
+(defn algorithmia [s]
+  (let [json->edn #(json/read-str % :key-fn keyword)
+        data (json->edn s)]))
