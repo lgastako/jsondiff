58963d22f880065660e5d5fa97a326eddb598372
John Evans <lgastako@gmail.com>
2015-03-13 03:38:15 -0700

    Initial.

---
+++ README.md
@@ -0,0 +1,9 @@
+# jsondiff
+
+An attempt at fulfilling this bounty:
+
+https://algorithmia.com/algorithms/lgastako/JSONDiff/edit
+
+## License
+
+Copyright © 2015 John Evans
---
+++ test/jsondiff/core_test.clj
@@ -0,0 +1,7 @@
+(ns jsondiff.core-test
+  (:require [clojure.test :refer :all]
+            [jsondiff.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
